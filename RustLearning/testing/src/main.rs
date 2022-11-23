d7c35a086d070f81c7f247d267c7c06e7958f58a
ryrysmiley <>
2022-11-22 22:15:56 -0800

    test automation

---
+++ RustLearning/testing/src/main.rs
@@ -0,0 +1,54 @@
+//test automation
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn larger_can_hold_smaller() {
+        // --snip--
+        let larger = Rectangle {
+            width: 8,
+            height: 7,
+        };
+        let smaller = Rectangle {
+            width: 5,
+            height: 1,
+        };
+
+        assert!(larger.can_hold(&smaller));
+    }
+
+    #[test]
+    fn smaller_cannot_hold_larger() {
+        let larger = Rectangle {
+            width: 8,
+            height: 7,
+        };
+        let smaller = Rectangle {
+            width: 5,
+            height: 1,
+        };
+
+        assert!(!smaller.can_hold(&larger));
+    }
+}
+
+fn main() {
+    // 1. Set up any needed data or state.
+    // 2. Call the code you want to test.
+    // 3. Assert the results are what you expect.
+
+}
+
---
+++ RustLearning/testing/target/debug/deps/testing-9d987e8f751adb27.d
@@ -0,0 +1,5 @@
+/Users/ryleyjue/Documents/CPSC_Courses/CPSC_354/FinalProject/RustLearning/testing/target/debug/deps/testing-9d987e8f751adb27: src/main.rs
+
+/Users/ryleyjue/Documents/CPSC_Courses/CPSC_354/FinalProject/RustLearning/testing/target/debug/deps/testing-9d987e8f751adb27.d: src/main.rs
+
+src/main.rs:
