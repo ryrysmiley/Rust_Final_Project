b82345eb589ef74d932022acbf812809b5e48a79
ryrysmiley <>
2022-12-16 21:16:58 -0800

    added user friendly features and handled invalid inputs

--- game_of_life/README.md
+++ game_of_life/README.md
@@ -11,11 +11,12 @@ The game of life is a cellular automaton. It is a zero player game, meaning that
 3. Run "cargo install rand" to install the rand crate
 4. Run "cargo build"
 5. Run "cargo run"
-6. Enter the desired game mode
-7. Enter the desired grid size
+6. Follow the instructions in the terminal
+
 # Expected Output
-1. The program will print the results in the terminal
-2. The program will write the results in a file called "output.txt"
+1. The program will print the results in the terminal.
+2. The program gives option to write to file.
+
 # Game Mode Descriptions
 ## Classic
 The classic game mode is the original game of life.
@@ -25,7 +26,5 @@ The mirror game mode is the same as the classic game mode, except that the walls
 The donut game mode is the same as the classic game mode, except that the walls of the grid are donuts. This means that cells on the edge of the are connected to cells on the opposite edge of the grid.
 
 # Error Handling
-## Invalid Game Mode
-If the user enters an invalid game mode, the program will print an error message and exit.
-## Invalid Grid Size
-If the user enters an invalid grid size, the program will print an error message and exit.
+All invalid inputs are handled by the program. If the user enters an invalid input, the program will just continue not breaking the program.
+The program has a limit to how many generations it will run. If the user enters a number greater than the limit, the program will just run the maximum number of generations (999).
--- game_of_life/src/gameoflife.rs
+++ game_of_life/src/gameoflife.rs
@@ -3,8 +3,8 @@ extern crate rand;
 use std::io::{self, Write};
 use std::collections::HashSet;
 use::colored::*;
-// use std::fs::File;
-// use std::io::Write;
+use std::fs::File;
+
 pub struct GoLGrid {
     grid: Vec<Vec<char>>,
     library: HashSet<Vec<Vec<char>>>,
@@ -39,18 +39,18 @@ impl GoLGrid {
         return self.gen_num;
     }
     //return grid as string
-    pub fn return_grid(&self) -> String {
-        let mut output = String::new();
-        for y in 0..self.height {
-            for x in 0..self.width {
-                output.push(self.grid[y][x]);
-            }
-            output.push('\n');
-        }
-        output.push_str(&format!("Generation: {}", self.gen_num));
-        output.push('\n');
-        output
-    }
+    // pub fn return_grid(&self) -> String {
+    //     let mut output = String::new();
+    //     for y in 0..self.height {
+    //         for x in 0..self.width {
+    //             output.push(self.grid[y][x]);
+    //         }
+    //         output.push('\n');
+    //     }
+    //     output.push_str(&format!("Generation: {}", self.gen_num));
+    //     output.push('\n');
+    //     output
+    // }
     //print return grid given the index
     pub fn print_grid_final(&self, index: usize) {
         let mut output = String::new();
@@ -60,7 +60,7 @@ impl GoLGrid {
             }
             output.push('\n');
         }
-        print!("{}{}", output, format!("Generation: {} \n", index + 1).blue().bold());
+        print!("{}{}", output, format!("Generation: {} of {}\n", index + 1, self.finished_grids.len()).blue().bold());
     }
 
     //randomly populate grid
@@ -200,45 +200,6 @@ impl GoLGrid {
     }
 }
 
-// game of life start game
-// pub fn start_game() {
-//     print!("Welcome to game of life! \n");
-//     print!("What gamemode would you like to play? \n");
-//     print!("1. Normal \n");
-//     print!("2. Wrap Around \n");
-//     print!("3. Mirror \n");
-//     let mut gamemode = String::new();
-//     io::stdin().read_line(&mut gamemode).expect("Failed to read line");
-//     let gamemode: u32 = gamemode.trim().parse().expect("Please type a number!");
-//     print!("What size would you like the grid to be? \n");
-//     print!("Width: \n");
-//     let mut width = String::new();
-//     io::stdin().read_line(&mut width).expect("Failed to read line");
-//     let width: u32 = width.trim().parse().expect("Please type a number!");
-//     print!("Height: \n");
-//     let mut height = String::new();
-//     io::stdin().read_line(&mut height).expect("Failed to read line");
-//     let height: u32 = height.trim().parse().expect("Please type a number!");
-//     let mut grid = GoLGrid::new(width as usize, height as usize);
-//     //populate the grid
-//     grid.randomize();
-//     print!("Starting Simulation... \n");
-//     //run generations
-//     //open file
-//     let mut file = File::create("output.txt").expect("Unable to create file");
-//     while grid.get_gen_num() < 1000 && !grid.check_match(){
-//         match gamemode {
-//             1 => grid.next_gen(),
-//             2 => grid.next_gen_wrap(),
-//             3 => grid.next_gen_mirror(),
-//             _ => grid.next_gen(),
-//         }
-//         print!("{}", grid.return_grid());
-//         file.write_all(grid.return_grid().as_bytes()).expect("Unable to write data");
-//     }
-//     print!("Simulation Complete! \n");
-// }
-
 // parameters are gamemode, width, height
 pub fn full_game() {
     //get user input and keep asking until valid
@@ -246,15 +207,19 @@ pub fn full_game() {
     print!("{}", "[Game of Life] \n".green().bold());
     //enter gamemode as integer
     //Ask until input is valid
+    #[allow(unused_mut)]
     let mut gamemode = String::new();
     print!("{}","[What gamemode would you like to play?] \n".red().bold());
     'gm: loop {
-        print!("1. Normal \n");
-        print!("2. Wrap Around \n");
-        print!("3. Mirror \n");
+        print!("{}",format!("1. Normal \n").white().bold());
+        print!("{}",format!("2. Wrap Around \n").white().bold());
+        print!("{}",format!("3. Mirror \n").white().bold());
         gamemode.clear();
+        #[allow(unused_must_use)]{
         io::stdout().flush();
+        }
         io::stdin().read_line(&mut gamemode).unwrap();
+        #[allow(unused_mut)]
         let mut gamemode = gamemode.trim().to_string();
         print!("{esc}c", esc = 27 as char);
         match gamemode.parse::<u32>() {
@@ -265,13 +230,17 @@ pub fn full_game() {
 
     //enter width and height as integers
     //Ask until input is valid
+    #[allow(unused_mut)]
     let mut width = String::new();
     print!("{}", "[Game of Life] \n".green().bold());
     print!("{}","[What width would you like the grid to be?] \n".red().bold());
     'w: loop {
         width.clear();
+        #[allow(unused_must_use)]{
         io::stdout().flush();
+        }
         io::stdin().read_line(&mut width).unwrap();
+        #[allow(unused_mut)]
         let mut width = width.trim().to_string();
         print!("{esc}c", esc = 27 as char);
         match width.parse::<u32>() {
@@ -279,13 +248,17 @@ pub fn full_game() {
             Err(..) => print!("{}{}","[Game of Life] \n".green().bold(), "[Please enter a valid width! (1-200)] \n".red().bold()),
         };
     }
+    #[allow(unused_mut)]
     let mut height = String::new();
     print!("{}", "[Game of Life] \n".green().bold());
     print!("{}","[What height would you like the grid to be?] \n".red().bold());
     'h: loop {
         height.clear();
+        #[allow(unused_must_use)]{
         io::stdout().flush();
+        }
         io::stdin().read_line(&mut height).unwrap();
+        #[allow(unused_mut)]
         let mut height = height.trim().to_string();
         print!("{esc}c", esc = 27 as char);
         match height.parse::<u32>() {
@@ -304,6 +277,9 @@ pub fn full_game() {
     //populate the grid
     grid.randomize();
     //run generations
+    print!("{esc}c", esc = 27 as char);
+    print!("{}", "[Game of Life] \n".green().bold());
+    print!("Generating Generations... \n");
     while grid.get_gen_num() < 1000 {
         match gamemode {
             1 => grid.next_gen(),
@@ -316,14 +292,87 @@ pub fn full_game() {
         }
     }
     //using the 3D vector allow a user to look at different generations
-    let genIndex = 0;
+    #[allow(unused_mut)]
+    let mut gen_index = 0;
     'v: loop {
         print!("{esc}c", esc = 27 as char);
         print!("{}", "[Game of Life] \n".green().bold());
         print!("{}{}{}{}{}{}{}{}{}",format!("[Gamemode: ").red().bold(),format!("{}",gamemode).red().bold().bold(),format!("]").red(),format!("[Width: ").red().bold(),format!("{}",width).red().bold().bold(),format!("]").red(),format!("[Height: ").red().bold(),format!("{}",height).red().bold().bold(),format!("]\n").red());
         //print the grid
-        grid.print_grid_final(genIndex);
+        grid.print_grid_final(gen_index);
         //user options
-        break;
+        print!("{}","[1 Prev] [2 Next] [3 First] [4 Last] [5 Custom] [6 Export to File] [7 Exit] \n".white().bold());
+        let mut input = String::new();
+        'i: loop {
+            input.clear();
+            #[allow(unused_must_use)]{
+            io::stdout().flush();
+            }
+            io::stdin().read_line(&mut input).unwrap();
+            #[allow(unused_mut)]
+            let mut input = input.trim().to_string();
+            match input.parse::<u32>() {
+                Ok(i) => if i > 0 && i < 8 {break 'i} else {continue 'v},
+                Err(..) => {continue 'v},
+            };
+        }
+        //convert input to integer
+        let input: u32 = input.trim().parse().expect("Error with conversion");
+        match input {
+            1 => if gen_index == 0 {continue 'v} else {gen_index-=1},
+            2 => if gen_index == grid.finished_grids.len()-1 {continue 'v} else {gen_index+=1},
+            3 => gen_index = 0,
+            4 => gen_index = grid.finished_grids.len()-1,
+            5 => {
+                //enter generation as integer
+                //Ask until input is valid
+                #[allow(unused_mut)]
+                let mut gen = String::new();
+                print!("{}", "[Enter Generation Number] \n".white().bold());
+                gen.clear();
+                #[allow(unused_must_use)]{
+                io::stdout().flush();
+                }
+                io::stdin().read_line(&mut gen).unwrap();
+                #[allow(unused_mut)]
+                let mut gen = gen.trim().to_string();
+                match gen.parse::<u32>() {
+                    Ok(i) => if i > 0 && i < (grid.finished_grids.len()+1).try_into().unwrap() {
+                        gen_index = i as usize - 1;
+                        continue 'v;
+                    } else {continue 'v},
+                    Err(..) => {continue 'v},
+                };
+            },
+            6 => {
+                //export to file called output.txt
+                let filename = "output.txt";
+                let mut file = File::create("output.txt").expect("Unable to create file");
+                //write to file
+                for i in 0..grid.finished_grids.len() {
+                    //grid to string
+                    let mut grid_string = String::new();
+                    for j in 0..grid.finished_grids[i].len() {
+                        for k in 0..grid.finished_grids[i][j].len() {
+                            grid_string.push(grid.finished_grids[i][j][k]);
+                        }
+                        grid_string.push_str("\n");
+                    }
+                    match file.write_all(format!("Generation {} \n", i+1).as_bytes()) {
+                        #[allow(unused_variables)]
+                        Err(why) => panic!("couldn't write to {}", filename),
+                        Ok(_) => println!("successfully wrote to {}", filename),
+                    }
+                    match file.write_all(grid_string.as_bytes()) {
+                        #[allow(unused_variables)]
+                        Err(why) => panic!("couldn't write to {}", filename),
+                        Ok(_) => println!("successfully wrote to {}", filename),
+                    }
+                }
+            }
+            7 => {break 'v},
+            _ => {continue 'v},
+        }
     }
+    print!("{esc}c", esc = 27 as char);
 }
