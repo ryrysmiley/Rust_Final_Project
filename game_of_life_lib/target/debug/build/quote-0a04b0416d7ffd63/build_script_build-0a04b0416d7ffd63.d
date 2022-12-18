692e58992bb961ce2720149e588dcfb661057edd
Ryley Jue <56055984+ryrysmiley@users.noreply.github.com>
2022-12-17 19:08:25 -0800

    Delete game_of_life_lib directory

--- game_of_life_lib/target/debug/build/quote-0a04b0416d7ffd63/build_script_build-0a04b0416d7ffd63.d
+++
@@ -1,5 +0,0 @@
-/Users/ryleyjue/Documents/CPSC_Courses/CPSC_354/FinalProject/game_of_life_lib/target/debug/build/quote-0a04b0416d7ffd63/build_script_build-0a04b0416d7ffd63: /Users/ryleyjue/.cargo/registry/src/github.com-1ecc6299db9ec823/quote-1.0.21/build.rs
-
-/Users/ryleyjue/Documents/CPSC_Courses/CPSC_354/FinalProject/game_of_life_lib/target/debug/build/quote-0a04b0416d7ffd63/build_script_build-0a04b0416d7ffd63.d: /Users/ryleyjue/.cargo/registry/src/github.com-1ecc6299db9ec823/quote-1.0.21/build.rs
-
-/Users/ryleyjue/.cargo/registry/src/github.com-1ecc6299db9ec823/quote-1.0.21/build.rs:
--- game_of_life_lib/target/debug/build/wasm-bindgen-shared-aeaa2e2da92e30c8/build_script_build-aeaa2e2da92e30c8.d
+++
@@ -1,5 +0,0 @@
-/Users/ryleyjue/Documents/CPSC_Courses/CPSC_354/FinalProject/game_of_life_lib/target/debug/build/wasm-bindgen-shared-aeaa2e2da92e30c8/build_script_build-aeaa2e2da92e30c8: /Users/ryleyjue/.cargo/registry/src/github.com-1ecc6299db9ec823/wasm-bindgen-shared-0.2.83/build.rs
-
-/Users/ryleyjue/Documents/CPSC_Courses/CPSC_354/FinalProject/game_of_life_lib/target/debug/build/wasm-bindgen-shared-aeaa2e2da92e30c8/build_script_build-aeaa2e2da92e30c8.d: /Users/ryleyjue/.cargo/registry/src/github.com-1ecc6299db9ec823/wasm-bindgen-shared-0.2.83/build.rs
-
-/Users/ryleyjue/.cargo/registry/src/github.com-1ecc6299db9ec823/wasm-bindgen-shared-0.2.83/build.rs:
